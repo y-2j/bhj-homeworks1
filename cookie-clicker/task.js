21ad61e4c3f979a50950c116ec7d54ffc510de18
y-2j <etozhedafomenko@gmail.com>
2019-10-18 12:53:45 +0400

    Выполнение задания

---
+++ cookie-clicker/task.js
@@ -0,0 +1,18 @@
+const element = document.getElementById("cookie");
+const click = document.getElementById("clicker");
+let time = new Date();
+function changeSizes() {
+    let last = new Date()
+    let difference = last - time;
+    let speed = (1 / difference) * 1000;
+    click.textContent = speed.toFixed(2);
+    const output = document.getElementById("clicker__counter");
+    output.textContent++ + 1;
+    if (element.width == 200) {
+        element.width = 300;
+    } else if (element.width == 300) {
+        element.width = 200;
+    }
+    return  time = new Date();
+};
+element.onclick = changeSizes;
