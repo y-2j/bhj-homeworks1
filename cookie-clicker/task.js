ef457c0d3c92e7b257a55ec6a2d75e0bf546455e
y-2j <etozhedafomenko@gmail.com>
2019-10-25 15:57:39 +0400

    правки по 2 заданию

--- cookie-clicker/task.js
+++ cookie-clicker/task.js
@@ -1,19 +1,11 @@
-const element = document.getElementById("cookie");
-const click = document.getElementById("clicker");
-let time = new Date();
-function changeSizes() {
-    let last = new Date()
-    let difference = last - time;
-    let speed = (1 / difference) * 1000;
-    click.textContent = speed.toFixed(2);
-    const output = document.getElementById("clicker__counter");
-    output.textContent++ + 1;
-    if (element.width == 200) {
-        element.width = 300;
-    } else if (element.width == 300) {
-        element.width = 200;
-    }
-    return  time = new Date();
-};
-element.onclick = changeSizes;
+const cookie = document.getElementById("cookie");
+const clickerCounter = document.getElementById("clicker__counter");
 
+cookie.onclick = function() {
+    let clicks = clickerCounter.textContent++;
+    if (clicks % 2 == 0) {
+        cookie.width = 150;
+    } else {
+        cookie.width = 200;
+    }
+}
